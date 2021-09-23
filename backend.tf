cb85737653afc5df2c56ce1a2157d16798e27474
David Anderson <david@mranderson.co.nz>
2021-09-23 13:36:33 +1200

    Initial commit

---
+++ backend.tf
@@ -0,0 +1,15 @@
+# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  backend "remote" {
+    organization = "example-org-5d36be"
+
+    workspaces {
+      name = "test-workspace"
+    }
+  }
+
+
+  required_version = ">= 1.0.6"
+}
+
